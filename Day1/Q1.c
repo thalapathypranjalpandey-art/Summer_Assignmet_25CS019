b7611db9d69b435422207a31d9aff2301e05cba3
thalapathypranjalpandey-art <thalapathypranjalpandey@gmail.com>
2026-06-10 13:28:45 +0530

    Update Q1.c

---
+++ Day1/Q1.c
@@ -0,0 +1,19 @@
+
+//Write a program to calculate sum of the N natural number 
+
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("Sum = %d\n", sum);
+
+    return 0;
+}

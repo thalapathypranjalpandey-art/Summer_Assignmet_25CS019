a9dc8db5f499bd6ce99f935edf9eb03983a57182
thalapathypranjalpandey-art <thalapathypranjalpandey@gmail.com>
2026-06-10 13:09:31 +0530

    Add files via upload

---
+++ Day1/tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{
+    int n, i, sum;
+    sum=0;
+    printf("enter the natural number:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+
+    printf("the sum is:%d\n", sum);
+
+    return 0;
+
+}
